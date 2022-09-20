b0ef5386c608f572eb66d73fb180024e3733d192
armagann12 <armagansisik@gmail.com>
2022-09-20 17:10:21 +0300

    Created creditCard Model

---
+++ InvoiceApi/Models/CreditCardModel.cs
@@ -0,0 +1,15 @@
+﻿namespace InvoiceApi.Models
+{
+    public class CreditCardModel
+    {
+        public int Id { get; set; }
+        public long Number { get; set; }
+        public string? UserName { get; set; }
+        public int CVC { get; set; }
+        public string? ValidDate { get; set; }
+        public string? BankName { get; set; }
+        public long Balance { get; set; }
+        public int UserModelId { get; set; }
+        public UserModel? UserModel { get; set; }
+    }
+}
